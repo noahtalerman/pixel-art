13bc776d7e1e3f2a8642eff79adfba19abaa14df
Noah Talerman <noahtal@umich.edu>
2019-06-24 19:40:35 -0500

    cleaned up js

--- static/scripts.js
+++ static/scripts.js
@@ -1,68 +1,45 @@
+// apply flexbox properties to container
 const container = document.querySelector('.container');
 container.style.display = 'flex';
 container.style.justifyContent = 'center';
 container.style.alignItems = 'center';
 
-const gridContainer = document.querySelector('.grid-container');
-gridContainer.style.display = 'flex';
-gridContainer.style.justifyContent = 'center';
-
+// hide directional text when user hovers over container
 container.addEventListener('mouseenter', function() {
     document.querySelector('.direction-text').style.opacity = '0';
 });
 
+const gridContainer = document.querySelector('.grid-container');
+gridContainer.style.display = 'flex';
+gridContainer.style.justifyContent = 'center';
+gridContainer.style.flexWrap = 'wrap';
+
 let numRows = 1;
+// gridSize passed through html scripts tag from app.py
 if (gridSize === 'error') {
 }
 else {
-    let height = (100 * (1 / gridSize)) + '%';
-    let width = height;
-    console.log(height);
+    // assigns width and height of squares according to respective fraction of grid size
+    let percentGridContainerWidth = (100 * (1 / gridSize)) + '%';
+    let width = percentGridContainerWidth
+    let height = percentGridContainerWidth;
     const createRow = () => {
         for (let i = 0; i < gridSize; i++) {
             let sqr = document.createElement('div');
             sqr.classList.add('square');
             sqr.classList.add('inactive');
-            sqr.style.setProperty('--element-height', height)
-            sqr.style.setProperty('--element-width', width)
-            // sqr.style.borderBottom = '0px';
-            // if (i !== 0){
-            //     sqr.style.borderLeft = '0px';
-            // }
+            sqr.style.setProperty('--element-height', height);
+            sqr.style.setProperty('--element-width', width);
             gridContainer.appendChild(sqr);
         }
     }
-
-    // const createLastRow = () => {
-    //     for (let i = 0; i < gridSize; i++) {
-    //         const sqr = document.createElement('div');
-    //         sqr.classList.add('square');
-    //         sqr.classList.add('inactive');
-    //         // if (i !== 0){
-    //         //     sqr.style.borderLeft = '0px';
-    //         // }
-    //         gridContainer.appendChild(sqr);
-    //     }
-    // }
     for (let i = 0; i < gridSize; i++) {
         createRow();
         ++numRows;
     }
-    // createLastRow();
 }
 
-let sqrWidth = document.querySelector('.square').getBoundingClientRect().width;
-let rowWidth = (numRows) * sqrWidth;
-gridContainer.style.flexWrap = 'wrap';
-
-const resizeRow = () => {
-    sqrWidth = document.querySelector('.square').getBoundingClientRect().width;
-    rowWidth = (numRows) * sqrWidth;
-}
-const body = document.querySelector('body');
-body.onresize = resizeRow;
-
-sqrs = document.querySelectorAll('.square');
+let sqrs = document.querySelectorAll('.square');
 
 sqrs.forEach(sqr => sqr.addEventListener('mouseenter', function(){
     let colors = ['#0000d6', '#1c00db', '#3d00e0', '#5300e8', '#6002ee',
@@ -72,7 +49,6 @@ sqrs.forEach(sqr => sqr.addEventListener('mouseenter', function(){
     this.style.setProperty('--element-border', random_color);
     this.classList.toggle('inactive');
     this.classList.toggle('active');
-    resizeRow();
 }));
 
 const clearBoard = () => {
@@ -80,7 +56,7 @@ const clearBoard = () => {
     sqrs.forEach(sqr => sqr.classList.add('inactive'));
 }
 
-refreshBtn = document.querySelector('.refresh-btn');
+const refreshBtn = document.querySelector('.refresh-btn');
 
 refreshBtn.addEventListener('click', function() {
     clearBoard();
@@ -89,8 +65,10 @@ refreshBtn.addEventListener('click', function() {
 const inputLabel = document.querySelector('.input__label-content');
 const inputBox = document.querySelector('.input');
 
+// text under input bar is toggled back when user clicks outside form
 document.addEventListener('click', function(e) {
-    let isClickInside = document.querySelector('.input__field').contains(e.target);
+    let inputField = document.querySelector('.input__field');
+    let isClickInside = inputField.contains(e.target);
     inputLabel.textContent = 'Enter an integer between 2 & 20';
     if (!isClickInside) {
         inputLabel.textContent = 'Height x Width';
